71b2a7c9f0028a4fa267b1b3dfab82d7bd7a1607
Jon <jonegeland@gmail.com>
2021-10-20 01:03:06 -0400

    animated track transitions

--- src/modules/spotify/SpotifyDisplay.tsx
+++ src/modules/spotify/SpotifyDisplay.tsx
@@ -1,8 +1,10 @@
 import * as React from "react";
+import { animated, useTransition, config } from "react-spring";
+
 import useSpotifyStore from "./SpotifyStore";
 
 import styles from "./SpotifyDisplay.mod.css";
-import { PlayerItem } from "../../spotify-api/APITypes";
+import type { PlayerItem } from "../../spotify-api/APITypes";
 
 function getArtists(track: PlayerItem) {
   const { artists } = track;
@@ -13,15 +15,27 @@ export default function SpotifyDisplay() {
   const state = useSpotifyStore((state) => state.playerState);
   const track = state?.item;
 
+  const transition = useTransition(track, {
+    from: { opacity: 0, y: 20 },
+    enter: { opacity: 1, y: 0 },
+    leave: { opacity: 0, y: 20 },
+    trail: 100,
+    config: config.slow,
+  });
+
   if (track == null) return <h1>Not Playing</h1>;
 
   return (
-    <div className={styles.container}>
-      <img className={styles.artwork} src={track.album.images[0].url} />
-      <div className={styles.info}>
-        <div className={styles.trackName}>{track.name}</div>
-        <div className={styles.artistName}>{getArtists(track)}</div>
-      </div>
+    <div className={styles.positioner}>
+      {transition(({ opacity, y }, item) => (
+        <animated.div key={item!.id} className={styles.container} style={{ opacity, y }}>
+          <img className={styles.artwork} src={item!.album.images[0].url} />
+          <div className={styles.info}>
+            <div className={styles.trackName}>{item!.name}</div>
+            <div className={styles.artistName}>{getArtists(item!)}</div>
+          </div>
+        </animated.div>
+      ))}
     </div>
   );
 }
